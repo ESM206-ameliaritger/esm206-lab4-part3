41b9f615ab30f5e6e213e61992cb7b643f9c22f7
ameliaritger <aritger@ucsb.edu>
2019-10-21 13:57:09 -0700

    subset data and add infant mortality graph

---
+++ part3.R
@@ -0,0 +1,20 @@
+# ---------------
+# Amelia 
+# ---------------
+library(tidyverse)
+library(janitor)
+library(here)
+
+#read in data
+db <- read_csv(here::here("data", "disease_burden.csv")) %>%
+  clean_names() %>%
+  rename(deaths_per_100k = death_rate_per_100_000)
+
+#subset data
+db_sub <- db %>%
+  filter(country_name %in% c("United States", "Japan", "Afghanistan", "Somalia")) %>% #subset for these specific countrys
+  filter(age_group=="0-6 days", sex=="Both") #subset only 0-6 days age group AND both sexes
+
+#create a basic line graph
+ggplot(data=db_sub, aes(x=year, y=deaths_per_100k)) +
+  geom_line(aes(color=country_name))
